2c63e23d8984cdbf573bf638c1976e1f47e982e9
James Provance <jb24x7@gmail.com>
2023-01-11 16:05:58 -0800

    properly displays quiz result

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,22 @@ function hideResult() {
   document.getElementById("moderate").setAttribute("class", "hidden");
   document.getElementById("liberal").setAttribute("class", "hidden");
   document.getElementById("conservative").setAttribute("class", "hidden");
-  document.getElementById("q2").setAttribute("class", "hidden");
-  document.getElementById("q3").setAttribute("class", "hidden");
-  document.getElementById("q4").setAttribute("class", "hidden");
+  // document.getElementById("q2").setAttribute("class", "hidden");
+  // document.getElementById("q3").setAttribute("class", "hidden");
+  // document.getElementById("q4").setAttribute("class", "hidden");
 }
 
 window.onload = function() {
-
-  
   hideResult();
   document.querySelector("form").onsubmit = function(event) {
-
     event.preventDefault();
 
+  if (countYes + countNo <= -1){
+    document.getElementById("conservative").removeAttribute("class");
+  } else if (countYes + countNo >= 1) {
+    document.getElementById("liberal").removeAttribute("class");
+  } else if (countYes + countNo == 0)
     document.getElementById("moderate").removeAttribute("class");
-
   };
 }
 
@@ -25,10 +26,12 @@ let buttonYes = document.getElementById("yes");
   countYes = 0;
 buttonYes.onclick = function() {
   countYes += 1;
+  buttonYes.innerHTML = "Yes:" + countYes;
 };
 
 let buttonNo = document.getElementById("no");
   countNo = 0;
 buttonNo.onclick = function() {
   countNo -= 1;
+  buttonNo.innerHTML = "NO:" + countNo;
 };
